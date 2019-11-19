b60f1ce30c8167d4841ffbaf482aa0d788779468
gusk94 <h3652k@gmail.com>
2019-11-19 15:20:19 +0900

    Finish API server

--- todo_back/todos/views.py
+++ todo_back/todos/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .models import Todo
+from .serializer import TodoSerializer, UserDetailSerializer
+User = get_user_model()
 
-# Create your views here.
+@api_view(['POST'])
+def todo_create(request):
+    # request.data = axios 의 body
+    serializer = TodoSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(['PUT', 'DELETE'])
+def todo_update_delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    if request.method == 'PUT':
+        serializer = TodoSerializer(instance=todo, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    else:
+        todo.delete()
+        # 요청 성공 / 컨텐츠는 없다는 걸 알려주는 status
+        return Response(status=204)
+
+
+@api_view(['GET'])
+def user_detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    serializer = UserDetailSerializer(instance=user)
+    return Response(serializer.data)
