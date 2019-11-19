1d5ccc9d9df5852cc7e93caa558550ff47fc9c69
gusk94 <h3652k@gmail.com>
2019-11-19 17:24:17 +0900

    Finish Todo

---
+++ todo-front/src/components/TodoInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo-input">
+    <form class="input-group mb-3" @submit.prevent="onSubmit">
+      <input type="text" class="form-control" v-model="title">
+      <button type="submit" class="btn btn-success">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoInput',
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('createTodo', this.title)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- todo-front/src/views/Home.vue
+++ todo-front/src/views/Home.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
-
+    <h1>Todo</h1>
+    <TodoInput @createTodo="createTodo"/>
+    <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+import TodoList from '../components/TodoList'
+import TodoInput from '@/components/TodoInput'
 import router from '@/router'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  components: {
+    TodoList,
+    TodoInput,
+  },
   methods: {
     // 사용자 로그인 유무 -> 로그인 x -> 로그인 페이지로 보냄
     checkLogginIn() {
@@ -21,10 +36,52 @@ export default {
         router.push('/login')        
       }
     },
+    // django API -> todos data
+    getTodo() {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP
+      const userId = jwtDecode(token).user_id
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      axios.get(`${SERVER_IP}/api/v1/users/${userId}/`, options)
+        .then(response=>{
+          this.todos = response.data.todo_set
+        })
+        .catch(error=>{
+          console.log(error)
+        })
+    },
+    createTodo(title) {
+      this.$session.start()
+      const token = this.$session.get('jwt')
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      const userId = jwtDecode(token).user_id
+      const data = {
+        title,
+        user: userId
+      }
+      axios.post(`${SERVER_IP}/api/v1/todos/`, data, options)
+        .then(response=>{
+          this.todos.push(response.data)
+        })
+        .catch(error=>{
+          console.log(error)
+        })
+    }
   },
   // Vue 가 화면에 그려지면 실행하는 함수
   mounted() {
     this.checkLogginIn()
+    this.getTodo()
   },
 }
 </script>
